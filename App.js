5fcb238d6c5c351a18d0c48c571b233e1dbf8dcd
SaurabhPandey16 <saurabhpandey@SAURABHs-LAPI.local>
2020-05-26 16:05:36 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+//import react in our code. 
+
+//Import react-navigation
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator} from 'react-navigation-stack';
+
+import FirstPage from './FirstPage';
+import SecondPage from './SecondPage';
+//import all the screens we are going to switch 
+const App = createStackNavigator({
+  //Constant which holds all the screens like index of any book 
+    FirstPage: { screen: FirstPage }, 
+    //First entry by default be our first screen if we do not define initialRouteName
+    SecondPage: { screen: SecondPage }, 
+  },
+  {
+    initialRouteName: 'FirstPage',
+  }
+);
+export default createAppContainer(App);
